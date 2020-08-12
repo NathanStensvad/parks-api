c16052ba8b205448475a0dd236d6888450b69ece
Nathan Stensvad <naterdoo6@gmail.com>
2020-08-12 00:45:51 +0000

    Fix HTML errors

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function displayResults(responseJson) {
     $('#results-list').append(
       `<li>
       <h3>Name: ${responseJson.data[i].fullName}</h3>
-      <p>Description ${responseJson.data[i].description}
+      <p>Description: ${responseJson.data[i].description}
       <p>Website: <a href="${responseJson.data[i].url}" target="_blank">${responseJson.data[i].name}</a></p> 
       <p>Address: <a href="${responseJson.data[i].directionsUrl}" target="_blank">${responseJson.data[i].addresses[0].line1}, ${responseJson.data[i].addresses[0].city}, ${responseJson.data[i].addresses[0].stateCode} ${responseJson.data[i].addresses[0].postalCode}</a></p>
       </li>`
@@ -15,7 +15,7 @@ function displayResults(responseJson) {
 }
 
 function getParks(parkName, maxResults) {
-  const url = "https://developer.nps.gov/api/v1/parks?stateCode=" + parkName + "&limit=" + maxResults + "&api_key=fwhv851Isj1gsxCqgrG5kjzZpyTcivnAAQfroDGG"
+  const url = "https://developer.nps.gov/api/v1/parks?stateCode=" + parkName + "&limit=" + maxResults + "&api_key=fwhv851Isj1gsxCqgrG5kjzZpyTcivnAAQfroDGG";
 
   fetch(url)
     .then(response => {
